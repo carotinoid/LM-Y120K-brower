62fe41d022c22174a69472bd75e547dc5355fe4b
gshs381102 <86095044+carotinoid@users.noreply.github.com>
2021-08-19 22:18:25 +0900

    Update act.js

--- act.js
+++ act.js
@@ -1,6 +1,6 @@
 function getsearch() {
   const input=document.getElementById("searchBox");
-  location.href=`https://www.google.co.kr/search?q=${input.value}`;
+  window.location.replace(`https://www.google.co.kr/search?q=${input.value}`);
 }
 
 document.querySelector("body input[type='button']").addEventListener('click',getsearch);
